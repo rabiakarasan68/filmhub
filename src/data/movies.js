8acce728bcad41b89914e2e34156d2d1dd73e1fb
Rabia KARASAN <36655893+rabiakarasan68@users.noreply.github.com>
2026-08-30 20:22:32 +0300

    Create movies.js

---
+++ src/data/movies.js
@@ -0,0 +1,31 @@
+const movies = [
+    {
+        id: 1,
+        title: "Inception",
+        year: 2010,
+        rating: 8.8,
+        image: "https://image.tmdb.org/t/p/w500/oYuLEt3zVCKq57qu2F8dT7NIa6f.jpg",
+        description:
+        "Bir hırsız, insanların rüyalarına girerek değerli bilgileri çalabilmektedir. Kendisine verilen son görev, bir fikri bir insanın zihnine yerleştirmektir."
+    },
+    {
+        id: 2,
+        title: "Interstellar",
+        year: 2014,
+        rating: 8.7,
+        image: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+        description:
+        "Dünya yaşanabilirliğini kaybetmeye başlarken bir grup astronot, insanlığın geleceğini kurtarabilecek yeni bir gezegen bulmak için uzaya gönderilir."
+    },
+    {
+        id: 3,
+        title: "The Dark Knight",
+        year: 2008,
+        rating: 9.0,
+        image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+        description:
+        "Batman, Gotham şehrini kaosa sürükleyen Joker adlı tehlikeli bir suçluyla mücadele etmek zorunda kalır."
+    }
+];
+
+export default movies;
